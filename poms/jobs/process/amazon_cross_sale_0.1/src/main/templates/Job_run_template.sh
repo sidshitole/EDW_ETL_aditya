f5a221ef7c36311ea9bb7b9c6c1db2438974b776
adipatnaik <aditya.patnaik@aretove.com>
2020-01-16 17:12:46 +0530

    cool

--- poms/jobs/process/amazon_cross_sale_0.1/src/main/templates/Job_run_template.sh
+++ poms/jobs/process/amazon_cross_sale_0.1/src/main/templates/Job_run_template.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
 ROOT_PATH=`pwd`
-java -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/log4j-1.2.16.jar:$ROOT_PATH/../lib/mysql-connector-java-5.1.30-bin.jar:$ROOT_PATH/amazon_cross_sale_0_1.jar: local_project.amazon_cross_sale_0_1.amazon_cross_sale  "$@" 
+java -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/RedshiftJDBC41-1.2.10.1009.jar:$ROOT_PATH/../lib/advancedPersistentLookupLib-1.2.jar:$ROOT_PATH/../lib/commons-collections-3.2.2.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/jboss-serialization.jar:$ROOT_PATH/../lib/log4j-1.2.15.jar:$ROOT_PATH/../lib/log4j-1.2.16.jar:$ROOT_PATH/../lib/mysql-connector-java-5.1.30-bin.jar:$ROOT_PATH/../lib/trove.jar:$ROOT_PATH/amazon_cross_sale_0_1.jar: edw_etl_aditya.amazon_cross_sale_0_1.amazon_cross_sale  "$@" 
