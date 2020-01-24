bf61cf7b6f096b469ee3b7e5f009ea5766046ab3
adipatnaik <aditya.patnaik@aretove.com>
2020-01-24 20:22:35 +0530

    Added job for fact ecomm amazon cannibalization

--- poms/jobs/process/amazon_cannibalization_job_process_0.1/src/main/templates/Job_run_template.bat
+++ poms/jobs/process/amazon_cannibalization_job_process_0.1/src/main/templates/Job_run_template.bat
@@ -1,3 +1,3 @@
 %~d0
 cd %~dp0
-java -Dtalend.component.manager.m2.repository=%cd%/../lib -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.17.jar;../lib/mysql-connector-java-5.1.30-bin.jar;../lib/redshift-jdbc42-no-awssdk-1.2.16.1027.jar;amazon_cannibalization_job_process_0_1.jar; edw_etl_aditya.amazon_cannibalization_job_process_0_1.amazon_cannibalization_job_process  %*
+java -Dtalend.component.manager.m2.repository=%cd%/../lib -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/commons-collections-3.2.2.jar;../lib/dom4j-1.6.1.jar;../lib/jboss-serialization.jar;../lib/log4j-1.2.17.jar;../lib/mysql-connector-java-5.1.30-bin.jar;../lib/redshift-jdbc42-no-awssdk-1.2.16.1027.jar;../lib/trove.jar;amazon_cannibalization_job_process_0_1.jar; edw_etl_aditya.amazon_cannibalization_job_process_0_1.amazon_cannibalization_job_process  %*
